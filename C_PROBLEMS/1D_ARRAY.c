adb5f394bbe1108365d385708df8e4d84d9b63f3
GAGANDEEP <gagandeep982006@gmail.com>
2025-10-08 00:02:23 +0530

    Update 1D_ARRAY.c

--- C_PROBLEMS/1D_ARRAY.c
+++ C_PROBLEMS/1D_ARRAY.c
@@ -1,1 +1,25 @@
+//Print the sum of the integers in the array.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+    int* arr,size,sum = 0;
+    scanf("%d",&size);
+    
+    arr = (int*)malloc(sizeof(int)*size);
+    
+    for(int i =0; i< size;i++){
+        scanf("%d",arr+i);
+        sum += (arr[i]);
+    }
+    
+    printf("%d",sum);
+    
+    free(arr);
+    arr = NULL;
+        
+    return 0;
+}
 
