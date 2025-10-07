fd910ceb9b398f6134c17457f402588a6a167c50
GAGANDEEP <gagandeep982006@gmail.com>
2025-10-07 23:57:43 +0530

    Create PRINTING-PATTERN.c

---
+++ C_PROBLEMS/PRINTING-PATTERN.c
@@ -0,0 +1,38 @@
+/*ex->5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5*/
+
+#include <stdio.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+    int arr[2*n-1];
+  	for(int i = 0; i<2*n-1;i++){
+        arr[i] = n;
+    }
+    
+    for(int i = 0,k = 2;i<2*n -1;i++){
+        if( i > 2*n/2){
+            k++;
+        }
+        for(int j = 0; j<(2*n -1);j++){
+            if((j>=i )&& (j<((2*n-1)-(i))) && i < (2*n)/2)
+                arr[j] = n-i;
+            else if (j >= ((2*n-1)-(i+1)) && (j < i))
+                arr[j] = k;
+            (j != 2*n-2)? printf("%d ",arr[j]) : printf("%d",arr[j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
